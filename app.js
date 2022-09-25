578e0f0fac02fd9c16d034cd25887e06cb840587
GrantAlaverdyan <100466261+GrantAlaverdyan@users.noreply.github.com>
2022-09-25 10:04:25 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', function (req, res) {
+    console.log(req.query)
+    const obj = {
+        address: "Moscow",
+        height: 185,
+        phone: 89779847748
+    }
+
+    res.send(obj)
+})
+
+app.post('/', function (req, res) {
+
+    console.log(req.body)
+    res.send("Работает")
+})
+
+
+app.listen(3000, console.log("Сервер успешно запущен на порту 3000"))
